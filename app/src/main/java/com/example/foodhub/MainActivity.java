f6bb67188b81a0f7001804c12ffb4e042eafe3bb
AzizMKachwala <akachwala24@gmail.com>
2023-10-25 11:17:07 +0530

    Day1

---
+++ app/src/main/java/com/example/foodhub/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.foodhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.LoginActivity;
+import com.example.SignUpActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView cvFacebook, cvGoogle;
+    Button btnSkip, btnEmailPhone;
+    TextView txtAlreadySignIn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cvFacebook = findViewById(R.id.cvFacebook);
+        cvGoogle = findViewById(R.id.cvGoogle);
+        btnSkip = findViewById(R.id.btnSkip);
+        btnEmailPhone = findViewById(R.id.btnEmailPhone);
+        txtAlreadySignIn = findViewById(R.id.txtAlreadySignIn);
+
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Skip Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cvFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "FACEBOOK LOGIN", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cvGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "GOOGLE LOGIN", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnEmailPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Email or Phone SignUp", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                finish();
+            }
+        });
+
+        txtAlreadySignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Already Signed Up. SignIn Now", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
